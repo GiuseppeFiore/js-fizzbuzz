eaeca876c5bab341218dcaa15247dd94abb58ef1
Giuseppe Fiore <giuseppe100891fiore@gmail.com>
2026-02-03 15:16:16 +0100

    correct pushing

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,13 +25,13 @@ for (let i = 1; i <= 100; i++) {
     console.log("FizzBuzz");
 
 // Se il numero è solo multiplo di 3 deve stampare in console "Fizz"
-// Con l'aggiunta dell'operatore relazionale "!=="  restitutisce "true" se il datro (stringa o numero) è uguale a tre
+// Con l'aggiunta dell'operatore relazionale "!==" restitutisce "Falso" perchè il dato (stringa o numero) è uguale a tre e non diverso. 
     } else if (i % 3 === 0 && i !== 3) {
     console.log("Fizz");
     }
 
 // Se il numero è solo multiplo di 5 deve stampare in console "buzz"
-// Con l'aggiunta dell'operatore relazionale "!=="  restitutisce "true" se il datro (stringa o numero) è uguale a cinque
+// Con l'aggiunta dell'operatore relazionale "!==" restitutisce "Falso" perchè il dato (stringa o numero) è uguale a cinque e non diverso.
     else if (i % 5 === 0 && i !== 5) {
     console.log("Buzz")
     }
